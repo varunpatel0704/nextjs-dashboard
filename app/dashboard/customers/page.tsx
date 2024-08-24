831abe32cfb98cb52473630cff8c39ad8a87da94
electro0704 <118429904+electro0704@users.noreply.github.com>
2024-08-24 15:59:50 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function customers() {
+  return (
+    <div>Customers page</div>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function invoices() {
+  return (
+    <div>Invoices page</div>
+  )
+}
+
+export default invoices
